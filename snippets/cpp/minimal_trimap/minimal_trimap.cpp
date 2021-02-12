c30cf511af1711fac3c3df7f80d0c978d7c32238
Luis Carlos Garcia-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2021-02-12 00:26:15 +0000

    Minimal C++ examples in different folders.

--- snippets/cpp/minimal_trimap/minimal_trimap.cpp
+++ snippets/cpp/minimal_trimap/minimal_trimap.cpp
@@ -7,8 +7,6 @@
 
 #include <opencv2/core/core.hpp>
 #include <grabcut/grabcut.h>
-//#include <opencv2/highgui/highgui.hpp>
-//#include <opencv2/imgproc/imgproc.hpp>
 
 int main(int argc, char **argv) {
   const std::string imagePath = "data/tool_512x409.png"; 
