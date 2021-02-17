85dae532e2371e24f8aef2c8276de36c89117261
Luis Carlos Garcia-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2021-02-17 20:54:25 +0000

    Preparing setup.py for the package.

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from setuptools import setup, dist
 from setuptools.command.install import install
 
@@ -5,16 +7,29 @@ class BinaryDistribution(dist.Distribution):
     def has_ext_modules(foo):
         return True
 
-setup(
-    name='grabcut',
-    version='0.0.1',
-    description="GrabCut implementation.",
-    author='Luis Carlos Garcia Peraza Herrera',
+setup(name='grabcut',
+    version='0.2.0',
+    description='GrabCut implementation for C++ with Python wrappers.',
+    url='https://github.com/luiscarlosgph/grabcut',
+    author='Luis C. Garcia-Peraza Herrera',
     author_email='luiscarlos.gph@gmail.com',
     license='MIT',
     packages=['grabcut'],
     package_data={'grabcut': ['_grabcut.so']},
     include_package_data=True,
     distclass=BinaryDistribution,
-    url='https://github.com/luiscarlosgph/grabcut',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: C',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+         ],
 )
